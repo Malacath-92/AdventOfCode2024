4569dd249b3487cfb0e98afd486153cf5ab76166
Mal <vingine.dev@gmail.com>
2024-12-02 12:11:07 +0100

    Cleanup some code and fix output names

--- 2024_1.py
+++ 2024_1.py
@@ -8,8 +8,6 @@ sample_data = """\
 3   4
 4   3
 2   5
-3   4
-9   4
 1   3
 3   9
 3   3"""
@@ -34,8 +32,9 @@ total_similarity = sum(similarities)
 print(f"Problem 2: {total_similarity}")
 
 if cli.visualize:
-    from manim import *
-    from scipy.spatial import Delaunay
+    import pathlib
+    import scipy.spatial
+    import manim
     import numpy
     import random
 
@@ -63,7 +62,7 @@ if cli.visualize:
     all_radii = [None] * len(all_points)
 
     # Note: These are not perfect radii, they are only guaranteed to not overlap
-    triangulation = Delaunay(all_points)
+    triangulation = scipy.spatial.Delaunay(all_points)
     indices, neighbours = triangulation.vertex_neighbor_vertices
     for i, point in enumerate(all_points):
         ith_neighbours = [
@@ -76,7 +75,7 @@ if cli.visualize:
         nearest_neighbour = min(ith_distances)
         all_radii[i] = nearest_neighbour / 2 * random.randrange(800, 1000) / 1000
 
-    class LocationsScene(Scene):
+    class LocationsScene(manim.Scene):
         def __init__(self):
             super().__init__()
 
@@ -87,26 +86,26 @@ if cli.visualize:
             self.right_dupes = {}
 
             def make_circle(point, radius, color, initial_offset):
-                circle = Circle(radius)
+                circle = manim.Circle(radius)
                 circle.move_to((point + [initial_offset, 0]).tolist() + [0])
                 circle.set_fill(color=color, opacity=0.8)
                 circle.set_stroke(None, opacity=0.0)
 
                 circle.generate_target()
-                circle.target.set_fill(color=GREEN, opacity=1.0)
+                circle.target.set_fill(color=manim.GREEN, opacity=1.0)
                 circle.target.move_to(point.tolist() + [0])
 
                 return circle
 
             for i in left_indices:
                 point, radius = all_points[i], all_radii[i]
-                self.left_circles[i] = make_circle(point, radius, PINK, -3.5)
+                self.left_circles[i] = make_circle(point, radius, manim.PINK, -3.5)
                 if i in right_indices:
                     self.left_dupes.add(i)
 
             for i in right_indices:
                 point, radius = all_points[i], all_radii[i]
-                self.right_circles[i] = make_circle(point, radius, BLUE, 3.5)
+                self.right_circles[i] = make_circle(point, radius, manim.BLUE, 3.5)
                 if i in left_indices:
                     if i not in self.right_dupes:
                         self.right_dupes[i] = []
@@ -132,13 +131,13 @@ if cli.visualize:
             self.wait()
 
         def create(self, list):
-            return [Create(c) for c in list.values()]
+            return [manim.Create(c) for c in list.values()]
 
         def fade_out_dupes(self, list, dupes):
-            return [FadeOut(c) for (i, c) in list.items() if i not in dupes]
+            return [manim.FadeOut(c) for (i, c) in list.items() if i not in dupes]
 
         def move_to_final(self, list, dupes):
-            return [MoveToTarget(c) for (i, c) in list.items() if i in dupes]
+            return [manim.MoveToTarget(c) for (i, c) in list.items() if i in dupes]
 
         def explode_dupes(self):
             explode = []
@@ -146,18 +145,18 @@ if cli.visualize:
                 point, radius = all_points[i], all_radii[i]
                 initial_angle = random.randrange(0, 360) / 180 * math.pi
                 for j, right_c in enumerate(dupes):
-                    right_c.set_fill(color=GREEN, opacity=0.5)
+                    right_c.set_fill(color=manim.GREEN, opacity=0.5)
                     right_c.move_to(point.tolist() + [0])
                     right_c.move_to(point.tolist() + [0])
 
-                    new_circle = Circle(radius * 0.3)
+                    new_circle = manim.Circle(radius * 0.3)
                     angle = initial_angle + j * math.pi * 2 / (len(dupes) + 1)
                     offset = [
                         2.3 * radius * math.cos(angle),
                         2.3 * radius * math.sin(angle),
                     ]
                     new_circle.move_to((point + offset).tolist() + [0])
-                    explode.append(Transform(right_c, new_circle))
+                    explode.append(manim.Transform(right_c, new_circle))
 
             return explode
 
@@ -166,11 +165,14 @@ if cli.visualize:
             for i, dupes in self.right_dupes.items():
                 point, radius = all_points[i], all_radii[i]
                 for _, right_c in enumerate(dupes):
-                    faded_circle = Circle(radius)
+                    faded_circle = manim.Circle(radius)
                     faded_circle.move_to(point.tolist() + [0])
-                    faded_circle.set_fill(color=GREEN, opacity=0.0)
+                    faded_circle.set_fill(color=manim.GREEN, opacity=0.0)
                     faded_circle.set_stroke(None, opacity=0.0)
-                    fade.append(Transform(right_c, faded_circle))
+                    fade.append(manim.Transform(right_c, faded_circle))
             return fade
 
+    manim.config.output_file = pathlib.Path(__file__).stem
+    if cli.sample:
+        manim.config.output_file = f"{manim.config.output_file}_sample"
     LocationsScene().render()
