11a3f2b3166f1bb55221f7fcc8421edecbc7d1dd
Mal <vingine.dev@gmail.com>
2024-12-02 12:30:01 +0100

    Day 2 Problem 2

--- 2024_2.py
+++ 2024_2.py
@@ -16,7 +16,7 @@ data = sample_data if cli.sample else aocd.data
 reports = [[int(n) for n in l.split()] for l in data.splitlines()]
 
 
-def is_report_safe(report):
+def is_report_safe(report, with_dampener=False):
     def sliding_window(iterable, n):
         iterator = iter(iterable)
         window = collections.deque(itertools.islice(iterator, n - 1), maxlen=n)
@@ -24,17 +24,45 @@ def is_report_safe(report):
             window.append(x)
             yield tuple(window)
 
+    def try_dampen(i):
+        # strip off left level
+        left_stripped = report.copy()
+        del left_stripped[i]
+        if is_report_safe(left_stripped):
+            return True
+
+        # strip off right level
+        right_stripped = report.copy()
+        del right_stripped[i + 1]
+        if is_report_safe(right_stripped):
+            return True
+
+        # strip off first level
+        first_stripped = report.copy()
+        del first_stripped[0]
+        if is_report_safe(first_stripped):
+            return True
+
+        # strip off second level
+        second_stripped = report.copy()
+        del second_stripped[1]
+        if is_report_safe(second_stripped):
+            return True
+
     last_diff = 0
-    for [lhs, rhs] in sliding_window(report, 2):
+    for i, [lhs, rhs] in enumerate(sliding_window(report, 2)):
         diff = rhs - lhs
         if abs(diff) < 1 or abs(diff) > 3:
-            return False
+            return try_dampen(i) if with_dampener else False
         if last_diff != 0 and last_diff / diff < 0:
-            return False
+            return try_dampen(i) if with_dampener else False
         last_diff = diff
     return True
 
 
 safe_reports = [report for report in reports if is_report_safe(report)]
 print(f"Problem 1: {len(safe_reports)}")
+
+safe_reports = [report for report in reports if is_report_safe(report, True)]
+print(f"Problem 2: {len(safe_reports)}")
 
