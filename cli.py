f09aaade33b079d9522b8d4a3ce033439eef6740
Mal <vingine.dev@gmail.com>
2024-12-07 12:07:17 +0100

    Fxi cli.py to allow running through cProfile

--- cli.py
+++ cli.py
@@ -1,16 +1,32 @@
 import argparse
-import __main__
+import inspect
+import pathlib
 
 if __name__ == "__main__":
     print("Can't run cli.py on its own...")
     exit(1)
 
+for frame in inspect.stack()[1:]:
+    if frame.filename[0] != "<":
+        prog_name = f'Advent of Code 2024 - Day {pathlib.Path(frame.filename).stem.split("_")[1]}'
+        break
+
 __parser__ = argparse.ArgumentParser(
-                    prog=f'Advent of Code 2024 - Day {__main__.__file__.split("_")[1].split(".")[0]}',
-                    description='Solves the Advent of Code problems of the given day')
-__parser__.add_argument('-v', '--verbose', action='store_true', help="Print out verbose info.")
-__parser__.add_argument('-i', '--visualize', action='store_true', help="Show problem visualization, if available.")
-__parser__.add_argument('-s', '--sample', action='store_true', help="Use sample data if available.")
+    prog=prog_name,
+    description="Solves the Advent of Code problems of the given day",
+)
+__parser__.add_argument(
+    "-v", "--verbose", action="store_true", help="Print out verbose info."
+)
+__parser__.add_argument(
+    "-i",
+    "--visualize",
+    action="store_true",
+    help="Show problem visualization, if available.",
+)
+__parser__.add_argument(
+    "-s", "--sample", action="store_true", help="Use sample data if available."
+)
 
 __args__ = __parser__.parse_args()
 
