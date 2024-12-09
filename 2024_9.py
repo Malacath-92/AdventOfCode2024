6ddb96e09f24a9eeb3e2f3846c2ac7cce350684d
Mal <vingine.dev@gmail.com>
2024-12-09 10:36:24 +0100

    Day 9 Part 2

--- 2024_9.py
+++ 2024_9.py
@@ -29,13 +29,64 @@ def checksum(blocks):
     return chk
 
 
-p1_blocks = blocks.copy()
+blocks = blocks.copy()
 last_non_empty_block = len(blocks) - 1
-while p1_blocks[-num_empty_blocks:].count(".") < num_empty_blocks:
-    first_empty_block = p1_blocks.index(".")
-    last_non_empty_block = get_next_non_empty_block(p1_blocks, last_non_empty_block)
-    p1_blocks[first_empty_block] = p1_blocks[last_non_empty_block]
-    p1_blocks[last_non_empty_block] = "."
+while blocks[-num_empty_blocks:].count(".") < num_empty_blocks:
+    first_empty_block = blocks.index(".")
+    last_non_empty_block = get_next_non_empty_block(blocks, last_non_empty_block)
+    blocks[first_empty_block] = blocks[last_non_empty_block]
+    blocks[last_non_empty_block] = "."
 
 
 print(f"Problem 1: {checksum(blocks)}")
+
+
+class Entry:
+    def __init__(self, size, idx):
+        self.size = size
+        self.id = idx // 2 if idx % 2 == 0 else -1
+
+    def __str__(self):
+        return str(self.id) if self.id >= 0 else "."
+
+
+def entry(size, idx):
+
+    return Entry(size, idx)
+
+
+disk = [entry(int(f), i) for i, f in enumerate(data)]
+file_idx = len(disk) - 1 if len(disk) % 2 != 0 else len(disk) - 2
+while file_idx > 0:
+    # Find index of empty block range that fits this file
+    empty_idx = 1
+    while empty_idx < file_idx and disk[empty_idx].size < disk[file_idx].size:
+        empty_idx += 2
+
+    # Move file if we found an index
+    if empty_idx < file_idx:
+        # Insert file entry in new position
+        disk[empty_idx : empty_idx + 1] = [
+            Entry(0, -1),
+            disk[file_idx],
+            Entry(disk[empty_idx].size - disk[file_idx].size, -1),
+        ]
+        file_idx += 2
+
+        # Remove file entry
+        disk[file_idx - 1].size += disk[file_idx].size
+        del disk[file_idx]
+
+        # Compact adjacent empty entries
+        if file_idx < len(disk) - 1:
+            disk[file_idx - 1].size += disk[file_idx].size
+            del disk[file_idx]
+
+        # Remove zero-length entries at the back of the list
+        if disk[-1] == 0:
+            del disk[-1]
+
+    file_idx -= 2
+
+defragmented_blocks = [str(block) for block in disk for _ in range(block.size)]
+print(f"Problem 2: {checksum(defragmented_blocks)}")
