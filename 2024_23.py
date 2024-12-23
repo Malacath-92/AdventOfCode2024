7fb3aba1d3c2ee54c9f7ba349da4fd7bed029fd0
Mal <vingine.dev@gmail.com>
2024-12-23 09:52:42 +0100

    Day 23 Part 2

---
+++ 2024_23.py
@@ -0,0 +1,71 @@
+import aocd
+import functools
+from typing import DefaultDict
+from tqdm import tqdm as progress
+
+import cli
+
+
+sample_data = """\
+kh-tc
+qp-kh
+de-cg
+ka-co
+yn-aq
+qp-ub
+cg-tb
+vc-aq
+tb-ka
+wh-tc
+yn-cg
+kh-ub
+ta-co
+de-co
+tc-td
+tb-wq
+wh-td
+ta-ka
+td-qp
+aq-cg
+wq-ub
+ub-vc
+de-ta
+wq-aq
+wq-vc
+wh-yn
+ka-de
+kh-ta
+co-tc
+wh-qp
+tb-vc
+td-yn"""
+data = sample_data if cli.sample else aocd.data
+
+connections = DefaultDict[str, list[str]](lambda: [])
+for l in data.splitlines():
+    [f, t] = l.split("-")
+    connections[f].append(t)
+    connections[t].append(f)
+
+################################################################################################
+# Problem 1
+t_devices = list(filter(lambda d: d[0] == "t", connections.keys()))
+
+three_networks: set[tuple[str, str, str]] = set()
+for first_device in t_devices:
+    for second_device in connections[first_device]:
+        for third_device in connections[second_device]:
+            if third_device == first_device:
+                continue
+
+            if first_device not in connections[third_device]:
+                continue
+
+            network = tuple(sorted([first_device, second_device, third_device]))
+            three_networks.add(network)
+
+print(f"Problem 1: {len(three_networks)}")
+
+
+################################################################################################
+# Problem 2
